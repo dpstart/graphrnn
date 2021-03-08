1c12d5b8803a28923efe8fa6aefea93c4beca5a7
dpstart <danielepaliotta96@gmail.com>
2021-03-08 11:56:45 +0100

    Moved to lightning

--- graphrnn/util.py
+++ graphrnn/util.py
@@ -1,7 +1,52 @@
+import os
+
+import torch
 import torch.nn.functional as F
+
 import numpy as np
 import pickle
 import networkx as nx
+
+def sample_sigmoid(y, sample, thresh=0.5, sample_time=2):
+    """
+    do sampling over unnormalized score
+    :param y: input
+    :param sample: Bool
+    :param thresh: if not sample, the threshold
+    :param sampe_time: how many times do we sample, if =1, do single sample
+    :return: sampled result
+    """
+
+    # do sigmoid first
+    y = torch.sigmoid(y)
+    # do sampling
+    if sample:
+        if sample_time > 1:
+            y_result = torch.autograd.Variable(
+                torch.rand(y.size(0), y.size(1), y.size(2))
+            )
+            # loop over all batches
+            for i in range(y_result.size(0)):
+                # do 'multi_sample' times sampling
+                for j in range(sample_time):
+                    y_thresh = torch.autograd.Variable(
+                        torch.rand(y.size(1), y.size(2))
+                    )
+                    y_result[i] = torch.gt(y[i], y_thresh).float()
+                    if (torch.sum(y_result[i]).data > 0).any():
+                        break
+                    # else:
+                    #     print('all zero',j)
+        else:
+            y_thresh = torch.autograd.Variable(
+                torch.rand(y.size(0), y.size(1), y.size(2))
+            )
+            y_result = torch.gt(y, y_thresh).float()
+    # do max likelihood based on some threshold
+    else:
+        y_thresh = Variable(torch.ones(y.size(0), y.size(1), y.size(2)) * thresh).cuda()
+        y_result = torch.gt(y, y_thresh).float()
+    return y_result
 
 
 def binary_cross_entropy_weight(
@@ -42,6 +87,10 @@ def get_graph(adj):
 
 
 # save a list of graphs
-def save_graph_list(G_list, fname):
+def save_graph_list(G_list, path, fname):
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+
     with open(fname, "wb") as f:
         pickle.dump(G_list, f)
